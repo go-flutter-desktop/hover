30b0d690f93291904b6d020fd6beedce90330ad3
Geert-Johan Riemer <geertjohan@geertjohan.net>
2020-04-21 21:47:56 +0200

    Fix flutter version detection (#104)

--- internal/flutterversion/version.go
+++ internal/flutterversion/version.go
@@ -1,6 +1,7 @@
 package flutterversion
 
 import (
+	"bytes"
 	"encoding/json"
 	"os"
 	"os/exec"
@@ -25,8 +26,25 @@ func readFlutterVersion() flutterVersionResponse {
 		log.Errorf("Failed to run %s: %v", log.Au().Magenta("flutter --version --machine"), err)
 		os.Exit(1)
 	}
+
+	// Read bytes from the stdout until we receive what looks like the start of
+	// a valid json object. This code may be removed when the following flutter
+	// issue is resolved. https://github.com/flutter/flutter/issues/54014
+	outputBuffer := bytes.NewBuffer(out)
+	for {
+		b, err := outputBuffer.ReadByte()
+		if err != nil {
+			log.Errorf("Failed to run %s: did not return information in json", log.Au().Magenta("flutter --version --machine"))
+			os.Exit(1)
+		}
+		if b == '{' {
+			outputBuffer.UnreadByte()
+			break
+		}
+	}
+
 	var response flutterVersionResponse
-	err = json.Unmarshal(out, &response)
+	err = json.NewDecoder(outputBuffer).Decode(&response)
 	if err != nil {
 		log.Errorf("Failed parsing json: %v", err)
 		os.Exit(1)
