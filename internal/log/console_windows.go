eaf2fd429af165ebb2c6cba205d934e9c7101451
altermark <49390312+altermark@users.noreply.github.com>
2020-06-20 14:19:07 +0200

    use internal/log for error logging

--- internal/log/console_windows.go
+++ internal/log/console_windows.go
@@ -3,7 +3,6 @@ package log
 import (
 	"golang.org/x/sys/windows"
 	"syscall"
-	"log"
 )
 
 // EnableColoredConsoleOutput sets flag that enables VT escape sequences
@@ -17,19 +16,19 @@ func EnableColoredConsoleOutput() {
 
 	console_handle, err = syscall.Open("CONOUT$", syscall.O_RDWR, 0)
 	if err != nil {
-		log.Println("Error getting console handle:", err)
+		Errorf("Error getting console handle: %v", err)
 		return
 	} 
 
 	var mode uint32
 	err = windows.GetConsoleMode(windows.Handle(console_handle), &mode)
 	if err != nil {
-		log.Println("Error getting console mode:", err)
+		Errorf("Error getting console mode: %v", err)
 		return
 	}
 	err = windows.SetConsoleMode(windows.Handle(console_handle), mode | windows.ENABLE_VIRTUAL_TERMINAL_PROCESSING)
 	if err != nil {
-		log.Println("Error setting console mode:", err)
+		Errorf("Error setting console mode: %v", err)
 		return
 	}
 }
