bf22bf810a0ce382286d9eef1bd9528ca74cd757
cosban <me@cosban.net>
2021-06-19 20:10:14 -0600

    FEAT: add profile mode
    FIX: ensure exactly one mode is set at a time

--- cmd/prepare-engine.go
+++ cmd/prepare-engine.go
@@ -1,14 +1,15 @@
 package cmd
 
 import (
+	"os"
+	"os/exec"
+	"runtime"
+
 	"github.com/go-flutter-desktop/hover/internal/build"
 	"github.com/go-flutter-desktop/hover/internal/config"
 	"github.com/go-flutter-desktop/hover/internal/enginecache"
 	"github.com/go-flutter-desktop/hover/internal/log"
 	"github.com/spf13/cobra"
-	"os"
-	"os/exec"
-	"runtime"
 )
 
 var (
@@ -16,14 +17,16 @@ var (
 	prepareEngineVersion string
 	prepareReleaseMode   bool
 	prepareDebugMode     bool
+	prepareProfileMode   bool
 	prepareBuildModes    []build.Mode
 )
 
 func init() {
 	prepareEngineCmd.PersistentFlags().StringVar(&prepareCachePath, "cache-path", enginecache.DefaultCachePath(), "The path that hover uses to cache dependencies such as the Flutter engine .so/.dll")
 	prepareEngineCmd.PersistentFlags().StringVar(&prepareEngineVersion, "engine-version", config.BuildEngineDefault, "The flutter engine version to use.")
-	prepareEngineCmd.PersistentFlags().BoolVar(&prepareDebugMode, "debug-mode", true, "Prepare the flutter engine for debug mode")
-	prepareEngineCmd.PersistentFlags().BoolVar(&prepareReleaseMode, "release-mode", false, "Prepare the flutter engine for release mode")
+	prepareEngineCmd.PersistentFlags().BoolVar(&prepareDebugMode, "debug", false, "Prepare the flutter engine for debug mode")
+	prepareEngineCmd.PersistentFlags().BoolVar(&prepareReleaseMode, "release", false, "Prepare the flutter engine for release mode.")
+	prepareEngineCmd.PersistentFlags().BoolVar(&prepareProfileMode, "profile", false, "Prepare the flutter engine for profile mode.")
 	prepareEngineCmd.AddCommand(prepareEngineLinuxCmd)
 	prepareEngineCmd.AddCommand(prepareEngineDarwinCmd)
 	prepareEngineCmd.AddCommand(prepareEngineWindowsCmd)
@@ -63,16 +66,27 @@ var prepareEngineWindowsCmd = &cobra.Command{
 }
 
 func initPrepareEngineParameters(targetOS string) {
+	validatePrepareEngineParameters(targetOS)
 	if prepareDebugMode {
 		prepareBuildModes = append(prepareBuildModes, build.DebugMode)
 	}
 	if prepareReleaseMode {
 		prepareBuildModes = append(prepareBuildModes, build.ReleaseMode)
 	}
-	validatePrepareEngineParameters(targetOS)
+	if prepareProfileMode {
+		prepareBuildModes = append(prepareBuildModes, build.ProfileMode)
+	}
 }
 
 func validatePrepareEngineParameters(targetOS string) {
+	if !prepareDebugMode && !prepareReleaseMode && !prepareProfileMode {
+		log.Errorf("No target mode set. Please select exactly one from: debug, profile, release.")
+		os.Exit(1)
+	}
+	if (prepareDebugMode && prepareReleaseMode) || (prepareDebugMode && prepareProfileMode) || (prepareReleaseMode && prepareProfileMode) {
+		log.Errorf("Multiple target modes set. Please select exactly one from: debug, profile, release.")
+		os.Exit(1)
+	}
 	if targetOS == "darwin" && runtime.GOOS != targetOS && prepareReleaseMode {
 		if path, err := exec.LookPath("darling"); err != nil || len(path) == 0 {
 			log.Errorf("To prepare the release flutter engine for darwin on linux, install darling from your package manager or https://www.darlinghq.org/")
